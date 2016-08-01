4d67b20efd36b65e653c927cf8b9d835877797d8
madaiqian <madaiqian@gmail.com>
2016-08-01 11:49:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,19 +23,19 @@ Size s = Size(720, 480);//Size(1440,960);
 
 int main(){
 
-	string str_head = "数据\\4\\";
+	string str_head = "data\\4\\";
 	string str[10] = { "11", "22", "00", "1", "2", "8" };
 	
 	Mat identity = Mat::eye(3, 3, CV_64F);
-    vector<vector<Point3d>>  obj_points;   //真实物体点集
-	vector<vector<Point2d> > img_points;   //成像平面点集
-	vector<Point3d> obj_temp;              //用于存放一张图的点集
-	double sq_sz = 41.27;                  //真实棋盘格方块宽度
+        vector<vector<Point3d>>  obj_points;   //real objects' point sets
+	vector<vector<Point2d> > img_points;   //image's point sets
+	vector<Point3d> obj_temp;              //real objects' point in one image
+	double sq_sz = 41.27;                  //real chessboard's square's width
 
 
 	/**********************************************
-	*  找棋盘格
-	*  若找到,则将点放入成像平面点集img_points
+	*  find chessboard's corners
+	*  if find, save points to img_points
 	**********************************************/
 	cout << "Image_list: ";
 	for (int i = 0; i < ImageNum; i++)
@@ -59,7 +59,7 @@ int main(){
 		waitKey(10);
 		//*/
 
-		//如果棋盘格寻找成功，建立成像平面点集
+		//if find chessboard's corners, save it to img_points
 		if (found)
 		{
 			vector<Point2d> img_temp;
@@ -73,7 +73,7 @@ int main(){
 		}
 	}
 
-	//构造物体三维坐标点集
+	//construct 3d real objects' point sets
 	for (int i = 0; i < ChessBoardHeight; i++) {
 		for (int j = 0; j < ChessBoardWidth; j++) {
 			obj_temp.push_back(Point3d(double(j * sq_sz), double(i * sq_sz), 0));
@@ -82,7 +82,7 @@ int main(){
 	for (int i = 0; i < checked_img_num; i++) obj_points.push_back(obj_temp);
 
 
-	//calibrate参数初始化
+	//calibrate param initialize
 	cv::Matx33d K,K2;
 	cv::Vec4d D;
 	int flag = 0;
@@ -90,16 +90,16 @@ int main(){
 	flag |= cv::fisheye::CALIB_CHECK_COND;
 	flag |= cv::fisheye::CALIB_FIX_SKEW;
 	//cout << "flag: "<<flag << endl;
-	//标定求出内参矩阵K和畸变系数矩阵D
+	//calibrate the intrinsic param matrix K and distorted coefficient matrix D 
 	double calibrate_error=fisheye::calibrate(obj_points, img_points, Size(img_width, img_height), K, D, noArray(), noArray(), flag, TermCriteria(3, 20, 1e-6));
 	getOptimalNewCameraMatrix(K, D, s, 1.0, s);
-	//调整参数会得到不同的K2,产生不同的矫正效果
+	//adjust the param will produce different K2, which influence the undistort effect
 	fisheye::estimateNewCameraMatrixForUndistortRectify(K, D, Size(720, 480), cv::noArray(), K2, 0.8, s ,1.0);
 	cout << endl;
 	cout << "K:" << K << endl;
 	cout << "D:" << D << endl;
 	cout << "calibrate_error:  "<< calibrate_error << endl;
-	/*数据
+	/*data
 	K = Matx33d(1.6833736075323174e+002, 0, 3.6025506560344564e+002, 0,
     1.4979904278996142e+002, 2.4054151911694728e+002, 0, 0, 1);
 		
@@ -108,7 +108,7 @@ int main(){
 	*/
 
 
-	//创建XML文件写出  
+	//write as XML file 
 	FileStorage fs("intrinsics.xml", FileStorage::WRITE);
 	Mat mat = Mat(K);
 	fs << "intrinsics" << mat;
@@ -117,7 +117,7 @@ int main(){
 	fs.release();
 
 
-	//鱼眼矫正后图像
+	//show the undistorted image
 	for (int i = 0; i <ImageNum; i++)
 	{
 		Mat output;// = Mat(Size(img_height, img_width), CV_8UC3);
